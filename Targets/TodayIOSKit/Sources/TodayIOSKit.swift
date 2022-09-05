ca4b888067b2e44592893f919083e586f7fcc0b1
rlarldud1234 <rlarldud6482@naver.com>
2022-09-05 22:35:22 +0900

    ⚙️ :: Tuist 프로젝트 생성

---
+++ Targets/TodayIOSKit/Sources/TodayIOSKit.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public final class TodayIOSKit {
+    public static func hello() {
+        print("Hello, from your Kit framework")
+    }
+}
---
+++ Targets/TodayIOSKit/Tests/TodayIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TodayIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TodayIOSKit", "TodayIOSKit")
+    }
+}

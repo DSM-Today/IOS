13d935964400356491299def4a0fbb65e4a8e7c9
rlarldud1234 <rlarldud6482@naver.com>
2022-09-12 19:45:39 +0900

    ⚙️ :: Project+DynamicFramework

---
+++ Tuist/ProjectDescriptionHelpers/Project Frame/Project+DynamicFramework.swift
@@ -0,0 +1,32 @@
+import ProjectDescription
+
+extension Project {
+
+    public static func dynamicFramework(
+        name: String,
+        platform: Platform,
+        infoplist: InfoPlist,
+        dependencies: [TargetDependency]
+    ) -> Project {
+        return Project(
+            name: name,
+            organizationName: todayOrganizationName,
+            settings: nil,
+            targets: [
+                Target(
+                    name: name,
+                    platform: .iOS,
+                    product: .framework,
+                    bundleId: "\(todayOrganizationName).\(name)",
+                    deploymentTarget: .iOS(
+                        targetVersion: "15.0",
+                        devices: .iphone
+                    ),
+                    infoPlist: infoplist,
+                    sources: ["Sources/**"],
+                    dependencies: dependencies
+                )
+            ]
+        )
+    }
+}

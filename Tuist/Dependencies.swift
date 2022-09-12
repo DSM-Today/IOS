532fbb2e44686594154156e30f21707776898ab2
rlarldud1234 <rlarldud6482@naver.com>
2022-09-12 19:45:13 +0900

    ⚙️ :: Dependencies

---
+++ Tuist/Dependencies.swift
@@ -0,0 +1,20 @@
+import ProjectDescription
+
+let depencies = Dependencies(
+    swiftPackageManager: SwiftPackageManagerDependencies([
+        // RxSwift
+        .remote(
+            url: "https://github.com/ReactiveX/RxSwift.git",
+            requirement: .upToNextMajor(from: "6.5.0")
+        ),
+        // SnapKit
+        .remote(
+            url: "https://github.com/SnapKit/SnapKit.git",
+            requirement: .upToNextMajor(from: "5.0.1")
+        ),
+        // Then
+        .remote(
+            url: "https://github.com/devxoul/Then.git",
+            requirement: .upToNextMajor(from: "3.0.0")
+        )
+    ]), platforms: [.iOS])

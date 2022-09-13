7c7166ec27a9cc056bdbe49d9c55131c60540305
rlarldud1234 <rlarldud6482@naver.com>
2022-09-13 11:43:26 +0900

    ⚰️ :: 이름 변경으로 인한 파일 삭제

--- Tuist/ProjectDescriptionHelpers/TargetScript.swift
+++
@@ -1,8 +0,0 @@
-import ProjectDescription
-
-extension TargetScript {
-    static let swiftlint = TargetScript.pre(
-        script: "../Scripts/SwiftLintRunScript.sh",
-        name: "SwiftLint"
-    )
-}

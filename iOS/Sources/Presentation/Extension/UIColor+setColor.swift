6920a1ad28a86003622ffea48b725dfa24fc9bf5
sian7563 <sian7563@naver.com>
2022-09-28 14:59:45 +0900

    ⚡️:: UIColor+setColor

---
+++ iOS/Sources/Presentation/Extension/UIColor+setColor.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(named: "Primary")!
+    static let blue1 = UIColor(named: "Blue1")!
+    static let blue2 = UIColor(named: "Blue2")!
+    static let blue3 = UIColor(named: "Blue3")!
+    static let blue4 = UIColor(named: "Blue4")!
+    static let blue5 = UIColor(named: "Blue5")!
+    static let green1 = UIColor(named: "Green1")!
+    static let yellow1 = UIColor(named: "Yellow1")!
+    static let brown1 = UIColor(named: "Brown1")!
+}

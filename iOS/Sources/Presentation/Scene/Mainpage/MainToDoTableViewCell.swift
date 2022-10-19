396d9437f96eaed289b4900376e824e23700b5d2
sian7563 <sian7563@naver.com>
2022-10-19 15:31:05 +0900

    ⚡️ :: MainToDoTableViewCell

---
+++ iOS/Sources/Presentation/Scene/Mainpage/MainToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainToDoTableViewCell.swift
+//  Today
+//
+//  Created by kimsian on 2022/10/12.
+//  Copyright © 2022 com.DSM-Today. All rights reserved.
+//
+
+import UIKit
+
+class MainToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

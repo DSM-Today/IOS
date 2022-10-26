97b2ce10ec372fa4b040ddab55adf3f039b3bf6e
sian7563 <sian7563@naver.com>
2022-10-26 15:53:17 +0900

    ⚡️ :: MusicPageUI 구현

---
+++ iOS/Sources/Presentation/Scene/MusicPage/MusicPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MusicPageViewController.swift
+//  Today
+//
+//  Created by kimsian on 2022/10/26.
+//  Copyright © 2022 com.DSM-Today. All rights reserved.
+//
+
+import UIKit
+
+class MusicPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

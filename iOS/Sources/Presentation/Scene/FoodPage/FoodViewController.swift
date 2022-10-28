206163f42c769301ade8887172bc8da45a3ca42c
sian7563 <sian7563@naver.com>
2022-10-28 23:29:57 +0900

    ⚡️ :: FoodView 구현

---
+++ iOS/Sources/Presentation/Scene/FoodPage/FoodViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FoodViewController.swift
+//  Today
+//
+//  Created by kimsian on 2022/10/28.
+//  Copyright © 2022 com.DSM-Today. All rights reserved.
+//
+
+import UIKit
+
+class FoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

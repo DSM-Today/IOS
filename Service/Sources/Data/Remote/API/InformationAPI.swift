0943af396131b823541aaed3d82cd1e9d628530b
rlarldud1234 <rlarldud6482@naver.com>
2022-11-07 23:46:04 +0900

    ⚡️ :: InformationAPI

---
+++ Service/Sources/Data/Remote/API/InformationAPI.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+import Moya
+
+enum InformationAPI {
+    case fetchInformationList
+    case fetchNews
+    case fetchLotto
+    case bookmarkNews
+    case bookmarkLotto
+    case deleteBookmarkNews
+    case deleteBookmarkLotto
+}
+
+extension InformationAPI: TodayAPI {
+    var domain: Domain {
+        return .information
+    }
+
+    var urlPath: String {
+        switch self {
+        case .fetchInformationList:
+            return "/list"
+        case .fetchNews, .bookmarkNews, .deleteBookmarkNews:
+            return "/news"
+        default:
+            return "/lotto"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .fetchInformationList, .fetchNews, .fetchLotto:
+            return .get
+        case .bookmarkNews, .bookmarkLotto:
+            return .post
+        default:
+            return .delete
+        }
+    }
+}

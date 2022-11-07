4297dd3ff2a6e613bb485943180dd30d2a9969e4
rlarldud1234 <rlarldud6482@naver.com>
2022-11-07 23:47:10 +0900

    ⚡️ :: UserAPI

---
+++ Service/Sources/Data/Remote/API/UserAPI.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+import Moya
+
+enum UserAPI {
+    case fetchBookmarkList
+    case editProfile(_ request: ProfileRequest)
+    case fetchProfile
+}
+
+extension UserAPI: TodayAPI {
+
+    var domain: Domain {
+        return .user
+    }
+
+    var urlPath: String {
+        switch self {
+        case .fetchBookmarkList:
+            return "/list"
+        default:
+            return "/profile"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .editProfile:
+            return .patch
+        default:
+            return .get
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .editProfile(let request):
+            return .requestJSONEncodable(request)
+        default:
+            return .requestPlain
+        }
+    }
+}

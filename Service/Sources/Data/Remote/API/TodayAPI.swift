bb9bd608f10e6b4e85b9a7b9ab787095ba3397b8
rlarldud1234 <rlarldud6482@naver.com>
2022-11-07 23:46:35 +0900

    ⚡️ :: TodayAPI

---
+++ Service/Sources/Data/Remote/API/TodayAPI.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+import Moya
+
+protocol TodayAPI: TargetType {
+    var domain: Domain { get }
+    var urlPath: String { get }
+}
+
+extension TodayAPI {
+
+    var baseURL: URL { URL(string: "http://3.38.110.184:8000")! }
+
+    var path: String {
+        return domain.uri + urlPath
+    }
+
+    var task: Task {
+        return .requestPlain
+    }
+
+    var validationType: ValidationType {
+        return .successCodes
+    }
+
+    var headers: [String: String]? {
+        return nil
+    }
+}
+
+enum Domain: String {
+    case auth
+    case user
+    case random
+    case suggest
+    case information
+}
+
+extension Domain {
+    var uri: String {
+        return "\(self.rawValue)"
+    }
+}

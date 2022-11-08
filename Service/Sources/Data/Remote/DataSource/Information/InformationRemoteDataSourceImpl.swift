8bc2fff0fbaa87effb08bb154a3f1a259ec83e28
rlarldud1234 <rlarldud6482@naver.com>
2022-11-08 23:45:00 +0900

    ⚡️ :: InformationRemoteDataSourceImpl

---
+++ Service/Sources/Data/Remote/DataSource/Information/InformationRemoteDataSourceImpl.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+import RxSwift
+
+final class InformationRemoteDataSourceImpl: RemoteDataSource<InformationAPI>, InformationRemoteDataSource {
+
+    static let shared = InformationRemoteDataSourceImpl()
+
+    func fetchInformationList() -> Single<[Subject]> {
+        return request(.fetchInformationList)
+            .map(SubjectInformationListResponse.self)
+            .map { $0.toDomain() }
+    }
+
+    func fetchNews() -> Single<News> {
+        return request(.fetchNews)
+            .map(NewsResponse.self)
+            .map { $0.toDomain() }
+    }
+
+    func fetchLotto() -> Single<Lotto> {
+        return request(.fetchLotto)
+            .map(LottoResponse.self)
+            .map { $0.toDomain() }
+    }
+
+    func bookmarkNews() -> Completable {
+        return request(.bookmarkNews)
+            .asCompletable()
+    }
+
+    func bookmarkLotto() -> Completable {
+        return request(.bookmarkLotto)
+            .asCompletable()
+    }
+
+    func deleteBookmarkNews() -> Completable {
+        return request(.deleteBookmarkNews)
+            .asCompletable()
+    }
+
+    func deleteBookmarkLotto() -> Completable {
+        return request(.deleteBookmarkLotto)
+            .asCompletable()
+    }
+}

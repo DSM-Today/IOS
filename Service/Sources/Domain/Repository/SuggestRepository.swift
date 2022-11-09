b109f2b0453359688acf2a350d2c48d99f7e0143
rlarldud1234 <rlarldud6482@naver.com>
2022-11-09 20:02:17 +0900

    ⚡️ :: SuggestRepository

---
+++ Service/Sources/Domain/Repository/SuggestRepository.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+import RxSwift
+
+protocol SuggestRepository {
+    func fetchSuggestList() -> Observable<[Subject]>
+    func fetchTodoList() -> Single<[Todo]>
+    func fetchBook() -> Single<Book>
+    func bookmarkBook() -> Completable
+    func deleteBookmarkBook() -> Completable
+    func fetchMovie() -> Single<Movie>
+    func bookmarkMovie() -> Completable
+    func deleteBookmarkMovie() -> Completable
+    func fetchMusic() -> Single<Music>
+    func bookmarkMusic() -> Completable
+    func deleteBookmarkMusic() -> Completable
+    func fetchWebtoon() -> Single<Webtoon>
+    func bookmarkWebtoon() -> Completable
+    func deleteBookmarkWebtoon() -> Completable
+    func fetchFood() -> Single<Food>
+    func bookmarkFood() -> Completable
+    func deleteBookmarkFood() -> Completable
+    func fetchCafeMenu() -> Single<CafeMenu>
+    func bookmarkCafeMenu() -> Completable
+    func deleteBookmarkCafeMenu() -> Completable
+}

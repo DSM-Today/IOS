0bcf42042fa75e80885658d30d1f97ce4fc4ed82
rlarldud1234 <rlarldud6482@naver.com>
2022-11-09 20:00:11 +0900

    ⚡️ :: Book

---
+++ Service/Sources/Domain/Entity/Suggest/Book.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public struct Book: Equatable {
+    public let imageUrl: URL
+    public let title: String
+    public let writer: String
+    public let score: String
+    public let reviewAmount: String
+    public let directUrl: URL
+    public let comment: String
+}

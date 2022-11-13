6c13eebedc0b76cfcbe590aa142dc650edda0bc0
rlarldud1234 <rlarldud6482@naver.com>
2022-11-13 22:44:34 +0900

    ⚡️ :: AuthRemoteDataSource

--- Service/Sources/Data/Remote/DataSource/Auth/AuthRemoteDataSource.swift
+++ Service/Sources/Data/Remote/DataSource/Auth/AuthRemoteDataSource.swift
@@ -4,6 +4,6 @@ import RxSwift
 
 protocol AuthRemoteDataSource: RemoteDataSource<AuthAPI> {
     func fetchClientID() -> Single<ClientID>
-    func googleLogin(idToken: String) -> Single<TokenResponse>
-    func refreshToken() -> Single<TokenResponse>
+    func googleLogin(idToken: String) -> Single<LoginResponse>
+    func refreshToken() -> Single<LoginResponse>
 }

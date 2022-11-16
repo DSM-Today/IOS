6fa7d7835a975a33decaf4c1a150add6688b564f
rlarldud1234 <rlarldud6482@naver.com>
2022-11-16 16:18:56 +0900

    ⚡️ :: RecommendCategoryViewModel

---
+++ iOS/Sources/Presentation/Scene/Category/Recommend/RecommendCategoryViewModel.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+import RxSwift
+import RxCocoa
+import RxFlow
+import Service
+
+class RecommendCategoryViewModel: ViewModel {
+
+    private let fetchSubjectSuggestListUseCase: FetchSubjectSuggestListUseCase
+
+    init(fetchSubjectSuggestListUseCase: FetchSubjectSuggestListUseCase) {
+        self.fetchSubjectSuggestListUseCase = fetchSubjectSuggestListUseCase
+    }
+
+    var steps = PublishRelay<Step>()
+    private var disposeBag = DisposeBag()
+
+    struct Input {
+        let viewAppear: Driver<Void>
+    }
+
+    struct Output {
+        let subjectList: BehaviorRelay<[Subject]>
+    }
+
+    func transform(_ input: Input) -> Output {
+        let subjectList = BehaviorRelay<[Subject]>(value: [])
+
+        input.viewAppear
+            .asObservable()
+            .flatMap {
+                self.fetchSubjectSuggestListUseCase.excute()
+            }
+            .subscribe(onNext: {
+                subjectList.accept($0)
+            })
+            .disposed(by: disposeBag)
+
+        return Output(subjectList: subjectList)
+    }
+
+}

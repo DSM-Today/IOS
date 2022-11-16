b8d6e82a39c9c421478d23e3787c28e62c52bd49
rlarldud1234 <rlarldud6482@naver.com>
2022-11-16 16:18:01 +0900

    ⚡️ :: InformationCategoryViewModel

---
+++ iOS/Sources/Presentation/Scene/Category/Information/InformationCategoryViewModel.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+import RxSwift
+import RxCocoa
+import RxFlow
+import Service
+
+class InformationCategoryViewModel: ViewModel, Stepper {
+
+    private let fetchSubjectInformationListUseCase: FetchSubjectInformationListuseCase
+
+    init(fetchSubjectInformationListUseCase: FetchSubjectInformationListuseCase) {
+        self.fetchSubjectInformationListUseCase = fetchSubjectInformationListUseCase
+    }
+
+    var steps = PublishRelay<Step>()
+    private var disposeBag = DisposeBag()
+
+    struct Input {
+        let viewAppear: Driver<Void>
+    }
+
+    struct Output {
+        let subjectList: BehaviorRelay<[Subject]>
+    }
+
+    func transform(_ input: Input) -> Output {
+        let subjectList = BehaviorRelay<[Subject]>(value: [])
+
+        input.viewAppear
+            .asObservable()
+            .flatMap {
+                self.fetchSubjectInformationListUseCase.excute()
+            }
+            .subscribe(onNext: {
+                subjectList.accept($0)
+            })
+            .disposed(by: disposeBag)
+
+        return Output(subjectList: subjectList)
+    }
+
+}

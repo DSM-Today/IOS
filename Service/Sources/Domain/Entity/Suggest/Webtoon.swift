998adc88bb8fd1fbd7bd75a607e74a7abdcfffeb
rlarldud1234 <rlarldud6482@naver.com>
2022-11-17 18:55:00 +0900

    ⚡️ :: 리스폰스 이름 변경

--- Service/Sources/Domain/Entity/Suggest/Webtoon.swift
+++ Service/Sources/Domain/Entity/Suggest/Webtoon.swift
@@ -1,11 +1,12 @@
 import Foundation
 
 public struct Webtoon: Equatable {
+    public let isAdult: String
+    public let isWorking: String
+    public let directUrl: URL
     public let title: String
+    public let imageUrl: URL
     public let writer: String
     public let comment: String
-    public let working: String
-    public let imageUrl: URL
     public let genre: String
-    public let directUrl: URL
 }

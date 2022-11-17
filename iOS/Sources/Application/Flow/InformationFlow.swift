a4d81395fe9ac48490a619cd49edc9dab7d38fa2
rlarldud1234 <rlarldud6482@naver.com>
2022-11-17 09:42:37 +0900

    ⚡️ :: InformationFlow

--- iOS/Sources/Application/Flow/InformationFlow.swift
+++ iOS/Sources/Application/Flow/InformationFlow.swift
@@ -10,7 +10,7 @@ class InformationFlow: Flow {
         return rootViewController
     }
 
-    private lazy var rootViewController = UINavigationController()
+    private lazy var rootViewController = app.informationCategoryViewController
 
     func navigate(to step: Step) -> FlowContributors {
         guard let step = step as? TodayStep else { return .none }
@@ -29,21 +29,25 @@ class InformationFlow: Flow {
 
 extension InformationFlow {
     private func navigateToInformationCategoryScene() -> FlowContributors {
-        let informationCetrgoryViewController = app.informationCategoryViewController
-        self.rootViewController.pushViewController(informationCetrgoryViewController, animated: true)
         return .one(flowContributor: .contribute(
-            withNextPresentable: informationCetrgoryViewController,
-            withNextStepper: informationCetrgoryViewController.viewModel
+            withNextPresentable: self.rootViewController,
+            withNextStepper: self.rootViewController.viewModel
         ))
     }
     private func navigateToNewsScene() -> FlowContributors {
         let newsViewController = app.newsViewController
-        self.rootViewController.pushViewController(newsViewController, animated: true)
-        return .one(flowContributor: .contribute(withNext: newsViewController))
+        self.rootViewController.navigationController?.pushViewController(newsViewController, animated: true)
+        return .one(flowContributor: .contribute(
+            withNextPresentable: newsViewController,
+            withNextStepper: newsViewController.viewModel
+        ))
     }
     private func navigateToLottoScene() -> FlowContributors {
         let lottoViewController = app.lottoViewController
-        self.rootViewController.pushViewController(lottoViewController, animated: true)
-        return .one(flowContributor: .contribute(withNext: lottoViewController))
+        self.rootViewController.navigationController?.pushViewController(lottoViewController, animated: true)
+        return .one(flowContributor: .contribute(
+            withNextPresentable: lottoViewController,
+            withNextStepper: lottoViewController.viewModel
+        ))
     }
 }

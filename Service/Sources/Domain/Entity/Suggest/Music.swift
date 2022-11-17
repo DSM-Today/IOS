70962263136a51dddbf1f85eab76454c9bc277fc
sian7563 <sian7563@naver.com>
2022-11-17 18:27:06 +0900

    ⚡️ :: 바뀐 API 수정

--- Service/Sources/Domain/Entity/Suggest/Music.swift
+++ Service/Sources/Domain/Entity/Suggest/Music.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 public struct Music: Equatable {
-    public let imageUrl: URL
+    public let imagePath: URL
     public let situation: String
     public let title: String
-    public let writer: String
+    public let songWriter: String
     public let publishedAt: Date
     public let directUrl: URL
 }

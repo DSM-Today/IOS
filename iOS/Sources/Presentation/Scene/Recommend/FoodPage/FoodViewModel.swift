fe19e7e1a9418699a870459124857843dca3e766
sian7563 <sian7563@naver.com>
2022-11-17 23:10:03 +0900

    ⚡️ :: ViewModel 구현

--- iOS/Sources/Presentation/Scene/Recommend/FoodPage/FoodViewModel.swift
+++ iOS/Sources/Presentation/Scene/Recommend/FoodPage/FoodViewModel.swift
@@ -1,21 +1,41 @@
 import Foundation
 
-import RxSwift
 import RxCocoa
 import RxFlow
+import RxSwift
 import Service
 
 class FoodViewModel: ViewModel, Stepper {
 
     var steps = PublishRelay<Step>()
 
+    private let fetchFoodUseCase: FetchFoodUseCase
+
+    init(fetchFoodUseCase: FetchFoodUseCase) {
+        self.fetchFoodUseCase = fetchFoodUseCase
+    }
+
     struct Input {
+        let getData: Driver<Void>
     }
 
     struct Output {
+        let food: PublishRelay<Food>
     }
 
+    private var disposeBag = DisposeBag()
+
     func transform(_ input: Input) -> Output {
-        return Output()
+        let food = PublishRelay<Food>()
+
+        input.getData.asObservable().flatMap {
+            self.fetchFoodUseCase.excute()
+        }.subscribe(onNext: {
+            food.accept($0)
+        }).disposed(by: disposeBag)
+
+        return Output(
+            food: food
+        )
     }
 }

bf9fc272e2b8726029294ca2cfd7a3f5734939e8
rlarldud1234 <rlarldud6482@naver.com>
2022-11-17 10:26:58 +0900

    ⚡️ :: MovieViewModel 추가해줌

---
+++ iOS/Sources/Presentation/Scene/Recommend/Movie/MovieViewModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+import RxSwift
+import RxCocoa
+import RxFlow
+import Service
+
+class MovieViewModel: ViewModel, Stepper {
+
+    var steps = PublishRelay<Step>()
+
+    struct Input {
+    }
+
+    struct Output {
+    }
+
+    func transform(_ input: Input) -> Output {
+        return Output()
+    }
+}

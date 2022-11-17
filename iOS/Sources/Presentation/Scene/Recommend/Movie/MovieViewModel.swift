bea65992fc22df7a504c9647005f83612def93cf
김태원 <rlaxodnjs0876@dsm.hs.kr>
2022-11-17 17:29:51 +0900

    ⚡️::MoviewViewModel

--- iOS/Sources/Presentation/Scene/Recommend/Movie/MovieViewModel.swift
+++ iOS/Sources/Presentation/Scene/Recommend/Movie/MovieViewModel.swift
@@ -7,15 +7,36 @@ import Service
 
 class MovieViewModel: ViewModel, Stepper {
 
+    private let fetchMovieUseCase: FetchMovieUseCase
+
+    init(fetchMovieUseCase: FetchMovieUseCase) {
+        self.fetchMovieUseCase = fetchMovieUseCase
+    }
+
     var steps = PublishRelay<Step>()
+    private let disposeBag = DisposeBag()
 
     struct Input {
+        let viewAppear: Driver<Void>
     }
 
     struct Output {
+        let movieValue: PublishRelay<Movie>
     }
 
     func transform(_ input: Input) -> Output {
-        return Output()
+        let movieValue = PublishRelay<Movie>()
+
+        input.viewAppear
+            .asObservable()
+            .flatMap {
+                self.fetchMovieUseCase.excute()
+            }
+            .subscribe(onNext: {
+                movieValue.accept($0)
+            })
+            .disposed(by: disposeBag)
+
+        return Output(movieValue: movieValue)
     }
 }

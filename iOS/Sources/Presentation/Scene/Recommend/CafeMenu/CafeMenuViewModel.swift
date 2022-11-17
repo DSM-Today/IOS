5dd8f93514fec3618e94e0bea607d9203b313783
김태원 <rlaxodnjs0876@dsm.hs.kr>
2022-11-17 19:13:52 +0900

    ⚡️:: CafeMenuViewModel

--- iOS/Sources/Presentation/Scene/Recommend/CafeMenu/CafeMenuViewModel.swift
+++ iOS/Sources/Presentation/Scene/Recommend/CafeMenu/CafeMenuViewModel.swift
@@ -6,16 +6,35 @@ import RxFlow
 import Service
 
 class CafeMenuViewModel: ViewModel, Stepper {
+    private let fetchCafeMenuUseCase: FetchCafeMenuUseCase
+
+    init(fetchCafeMenuUseCase: FetchCafeMenuUseCase) {
+        self.fetchCafeMenuUseCase = fetchCafeMenuUseCase
+    }
 
     var steps = PublishRelay<Step>()
+    private let disposeBag = DisposeBag()
 
     struct Input {
+        let viewAppear: Driver<Void>
     }
 
     struct Output {
+        var cafeMenuValue: PublishRelay<CafeMenu>
     }
 
     func transform(_ input: Input) -> Output {
-        return Output()
+        let cafeMenuValue = PublishRelay<CafeMenu>()
+
+        input.viewAppear
+            .asObservable()
+            .flatMap {
+                self.fetchCafeMenuUseCase.excute()
+            }
+            .subscribe(onNext: {
+                cafeMenuValue.accept($0)
+            })
+            .disposed(by: disposeBag)
+        return Output(cafeMenuValue: cafeMenuValue)
     }
 }

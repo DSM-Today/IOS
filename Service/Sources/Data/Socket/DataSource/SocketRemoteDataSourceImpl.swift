ccae1710a7a9abc23a2cea99ffbd807c3fc39af8
rlarldud1234 <rlarldud6482@naver.com>
2022-12-02 11:32:19 +0900

    ⚡️ :: 태원아 로그 찍어

--- Service/Sources/Data/Socket/DataSource/SocketRemoteDataSourceImpl.swift
+++ Service/Sources/Data/Socket/DataSource/SocketRemoteDataSourceImpl.swift
@@ -1,43 +1,25 @@
 import Foundation
 
 import RxSwift
+import SocketIO
 
 class SocketRemoteDataSourceImpl: SocketRemoteDataSource {
 
     private let socketIOManager = SocketIOManager.shared
 
     func fetchRoomId() -> Observable<String> {
-        return Observable<String>.create { observable in
-            self.socketIOManager.on(.connectResponse) { (data, _) in
-                do {
-                    let cur = try JSONSerialization.data(withJSONObject: data[0])
-                    let roomId = try JSONDecoder().decode(RoomIdResponse.self, from: cur)
-                    observable.onNext(roomId.roomId)
-                } catch {
-                    print(error)
-                }
-            }
-            return Disposables.create()
-        }
+        return socketIOManager.on(.connectResponse)
+            .flatMap { _ in Observable<String>.just("") }
     }
 
     func sendMessage(roomId: String, message: String) {
         self.socketIOManager.emit(.sendMessage(roomId: roomId, data: message))
     }
 
+    // swiftlint:disable line_length
     func message() -> Observable<Chat> {
-        return Observable<Chat>.create { observable in
-            self.socketIOManager.on(.message) { data, _ in
-                do {
-                    let cur = try JSONSerialization.data(withJSONObject: data[0])
-                    let chat = try JSONDecoder().decode(ChatResponse.self, from: cur)
-                    observable.onNext(chat.toDomain())
-                } catch {
-                    print(error)
-                }
-            }
-            return Disposables.create()
-        }
+        socketIOManager.on(.message)
+            .flatMap { _ in return Observable<Chat>.just(.init(content: "", imageUrl: URL(string: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg")!, sender: ""))}
     }
 
     func leave() {
--- Service/Sources/Data/Socket/Socket/SocketIOManager.swift
+++ Service/Sources/Data/Socket/Socket/SocketIOManager.swift
@@ -1,23 +1,36 @@
 import Foundation
 
 import SocketIO
+import RxSwift
 
-public final class SocketIOManager {
+public final class SocketIOManager: NSObject {
 
     public static let shared = SocketIOManager()
 
     private let manager = SocketManager(
-        socketURL: URL(string: "http://43.201.53.240:8000")!,
+        socketURL: URL(string: "https://03ea-211-36-142-183.jp.ngrok.io")!,
         config: [
             .log(true),
-            .extraHeaders(["Authorization": "Bearer \(KeychainTask.shared.fetchAccessToken() ?? "")"])
+            .compress,
+            .extraHeaders(["Authorization": "Bearer \(KeychainTask.shared.fetchAccessToken() ?? "")"]),
+            .forceWebsockets(true)
         ]
     )
 
     private var socket: SocketIOClient!
 
-    init() {
-        socket = self.manager.socket(forNamespace: "/chat")
+    override init() {
+        super.init()
+        self.socket = self.manager.socket(forNamespace: "/chat")
+        self.socket.connect()
+        self.socket.on(clientEvent: .connect) { data, _ in
+            print("-----")
+            print(data)
+        }
+        self.socket.on("connect_response") { data, _ in
+            print("!!!!!!!")
+            print(data)
+        }
     }
 
     public func establishConnection() {
@@ -28,11 +41,17 @@ public final class SocketIOManager {
         self.socket.disconnect()
     }
 
-    public func on(_ api: TodaySocket, callback: @escaping (([Any], SocketAckEmitter) -> Void)) {
-        self.socket.on(api.event, callback: callback)
+    public func on(_ api: TodaySocket) -> Observable<[Any]> {
+        return Observable<[Any]>.create { observar in
+//            self.socket.on(api.event) { data, _ in
+//                observar.onNext(data)
+//            }
+            return Disposables.create()
+        }
     }
 
     public func emit(_ api: TodaySocket) {
+        print("!!!!!")
         self.socket.emit(api.event, api.items ?? [])
     }
 
